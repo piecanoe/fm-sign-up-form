546b36741f63ce8f34599832e3a429111dd168dc
Captain Du <karenkaydu@gmail.com>
2023-10-31 00:26:00 -0700

    Change CSS and change JS

--- js/main.js
+++ js/main.js
@@ -1,70 +1,11 @@
-const form = document.getElementById('form')
-const firstName = document.getElementById('fname')
-const lastName = document.getElementById('lname')
-const email = document.getElementById('email')
-const password = document.getElementById('password')
+document.getElementById('submit').addEventListener('click', checkForBlank);
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
 
-    validateInputs()
-})
-
-const setError = (element, message) => {
-    const inputControl = element.parentElement
-    const errorDisplay = inputControl.querySelector('.error')
-
-    errorDisplay.innerText = message
-    inputControl.classList.add('error')
-    inputControl.classList.remove('success')
-}
-
-const setSuccess = element => {
-    const inputControl = element.parentElement
-    const errorDisplay = inputControl.querySelector('.error')
-
-    errorDisplay.innerText = message
-    inputControl.classList.add('success')
-    inputControl.classList.remove('error')
-}
-
-const validEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase())
-
-}
-
-const validateInputs = () => {
-    const firstNameValue = firstName.ariaValueMax.trim()
-    const lastNameValue = lastName.ariaValueMax.trim()
-    const emailValue = email.ariaValueMax.trim()
-    const passwordValue = password.ariaValueMax.trim()
-
-    if(firstNameValue === ''){
-        setError(firstName, 'First Name cannot be empty')
+function checkForBlank (){
+    if (document.getElementsByClassName('input').display == "none") {
+            document.getElementsByClassName('error') = "block"
+            document.getElementsByClassName('input').style.borderColor = "red";
     }else{
-        setSuccess(firstName)
-    }
-
-    if(lastNameValue === ''){
-        setError(lastName, 'Last Name cannot be empty')
-    }else{
-        setSuccess(lastName)
-    }    
-
-    if(emailValue === ''){
-        setError(email, 'Looks like this is not an email')
-    }else if (!validEmail(emailValue)){
-        setError(email, 'Looks like this is not an email')
-    }else{
-        setSuccess(email)
-    }
-
-    if(passwordValue === ''){
-        setError(password, 'Password cannot be empty')
-    }else if (passwordValue.length < 8){
-        setError(password, 'Password must be at least 8 characters.')
-    }else{
-        setSuccess(password)
+        document.getElementsByClassName('error') = "none"
     }
 }
